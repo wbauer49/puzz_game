988eb25064471b5885edafda474ff55095a11d36
wbauer49 <will.awesomeness.bauer@gmail.com>
2023-10-14 18:22:55 -0700

    one more try

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pathlib
 import pygbag.app
 
 
-def app(start_fn, environ):
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'application/octet-stream')])
     sys.argv = [pathlib.Path(__file__).parent, "main.py"]
     pygbag.app.main()
+    return []
